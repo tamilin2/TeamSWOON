249d84eefd6bf97c7bf37a8607c17f5440262d5b
Chaz-Keck <cmkeck@ucsd.edu>
2017-02-21 22:21:29 -0800

    Add files via upload

---
+++ ClubPage/clubpage.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    function clubProfile(name, email, phone, site, descrip, meetings) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.site = site;
+        this.description = descrip;
+        this.meetings = meetings
+    }
+    function meeting(startday, starttime, startseg, endday, endtime, endseg) {
+        this.startday = startday;
+        this.starttime = starttime;
+        this.startseg = startseg;
+        this.endday = endday;
+        this.endtime = endtime;
+        this.endseg = endseg;
+        this.toString = function() {
+            return startday + " " + starttime + startseg + " - " + endday + " " + endtime + endseg;
+        }
+    }
+    $('#clubname').change(function() {
+        var name = $("input[name=clubname]").val();
+    });
+    
+    var dayfrom;
+    var timefrom;
+    var dayto;
+    var timeto;
+    
+    $("#dayfrom").change(function() {
+        dayfrom = $("#dayfrom option:selected").text();
+        
+    });
+    
+    $("#timefrom").change(function() {
+        timefrom = $("#timefrom option:selected").text();
+    });
+    
+    $("#dayto").change(function() {
+        dayto = $("#dayto option:selected").text();
+    });
+    
+    $("#timeto").change(function() {
+        timeto = $("#timeto option:selected").text();
+    });
+    
+    $('#meetbtn').click(function(){
+        var meetstring = "<p>" + dayfrom + " " + timefrom + " - " + dayto + " " + timeto + "</p>";
+        $("#meetingheader").append(meetstring);
+    });
+});
