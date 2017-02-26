6e4d882c5fef714e9851684648ed38a039c873ef
Chaz-Keck <cmkeck@ucsd.edu>
2017-02-26 10:03:03 -0800

    Add files via upload

--- ClubPage/clubpage.js
+++ ClubPage/clubpage.js
@@ -1,48 +1,52 @@
-$(document).ready(function() {
-    function clubProfile(name, email, phone, site, descrip, meetings) {
+$(document).ready(function () {
+    var meetings = [];
+    var categories = [];
+    var formdata;
+    function ClubProfile(name, email, phone, site, description, categories, meetings) {
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.site = site;
-        this.description = descrip;
-        this.meetings = meetings
+        this. description = description;
+        this.categories = categories;
+        this.meetings = meetings;
     }
-    function meeting(startday, starttime, startseg, endday, endtime, endseg) {
+    
+    function Meeting(startday, starttime, endday, endtime){
         this.startday = startday;
         this.starttime = starttime;
         this.endday = endday;
-        this.endtime = endtime;        
-        }
+        this.endtime = endtime;
+    }
     
-    var
-    $('#clubname').change(function() {
-        var name = $("input[name=clubname]").val();
-    });
-    
-    var dayfrom;
-    var timefrom;
-    var dayto;
-    var timeto;
-    
-    $("#dayfrom").change(function() {
-        dayfrom = $("#dayfrom option:selected").text();
+    $(".meetfield").change(function() {
+        var incomplete = false;
+        $("form select").each(function () {
         
+            if ($(this).val() == "na") {
+                incomplete = true;
+            }
+        });
+        
+        if(incomplete){
+            $("#meetbtn").addClass("disabled");
+        } else {
+            $("#meetbtn").removeClass("disabled");
+        }
+    })
+    
+    $("#meetbtn").click(function() {
+        var meeting = $(".meetfield").serializeArray();
+        var meet = new Meeting(meeting[dayfrom], meeting[timefrom], meeting[dayto], meeting[timeto]);
+        meetings.push(meet);
+        console.log(meet);
     });
     
-    $("#timefrom").change(function() {
-        timefrom = $("#timefrom option:selected").text();
+    $("#submitbtn").click(function() {
+        formdata = $(".tfield").serializeArray();
+        console.log(formdata);
+        alert("Your profile has been created!");
     });
     
-    $("#dayto").change(function() {
-        dayto = $("#dayto option:selected").text();
-    });
     
-    $("#timeto").change(function() {
-        timeto = $("#timeto option:selected").text();
-    });
-    
-    $('#meetbtn').click(function(){
-        var meetstring = "<p>" + dayfrom + " " + timefrom + " - " + dayto + " " + timeto + "</p>";
-        $("#meetingheader").append(meetstring);
-    });
 });
