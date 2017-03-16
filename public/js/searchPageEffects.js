16826e50248f5ec4d8fb76f2cf949b88a7366b17
Kevin Edington <kedingto@ucsd.edu>
2017-03-16 00:31:44 -0700

    changed filter buttons to bold when selected because when they change the same color they are its not clear they are clicked

--- public/js/searchPageEffects.js
+++ public/js/searchPageEffects.js
@@ -5,14 +5,15 @@ $(document).ready(function() {
         $(".list-group-item").css("background-color", "");
         $(".SubCategories").attr("checked", false);
     });
-    
+
     $(".list-group-item").click(function() {
         if (!$(this).children(".SubCategories").prop("checked")) {
             $(this).children(".SubCategories").attr("checked", true);
-            $(this).css("background-color", "whitesmoke");
+            $(this).children(".interestlabel").css("font-weight", "bold");
         }
         else {
             $(this).children(".SubCategories").attr("checked", false);
+            $(this).children(".interestlabel").css("font-weight", "normal");
             $(this).css("background-color", "");
         }
     });
