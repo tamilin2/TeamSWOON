3f5a44482d974c6d8a1e4a6ee15127eec3aad091
Jeffrey <jyen@ucsd.edu>
2017-03-16 16:37:08 -0700

    Added condition that a user should be logged in when editing a club

--- routes/users.js
+++ routes/users.js
@@ -148,7 +148,7 @@ router.get('/userProfilePage', authenticator.ensureLoggedIn , function (req, res
 /**
  * Loads edit club profile if user is creator
  */
-router.get('/editClubProfile',function (req, res) {
+router.get('/editClubProfile', authenticator.ensureLoggedIn, function (req, res) {
     res.render('pages/editClubProfile', {club: req.session.club, interests: req.session.interests, schedules: req.session.schedules});
 });
 /**
